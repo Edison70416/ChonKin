53a8951993ed8c5b8c363eaa77e0f81a8bc8de47
Edison70416 <65762864+Edison70416@users.noreply.github.com>
2023-08-22 22:04:17 +0930

    Update setup_base.sql

--- setup_base.sql
+++ setup_base.sql
@@ -1,20 +1,14 @@
--- Create the test1 database
 CREATE DATABASE IF NOT EXISTS test1;
-
--- Use the test1 database
 USE test1;
 
 -- Create the table tab1
-DROP TABLE IF EXISTS tab1;  -- This line ensures that if the table already exists, it's dropped first to avoid errors
+DROP TABLE IF EXISTS tab1; 
 CREATE TABLE tab1 (
     recordID INT(11) NOT NULL,
     name VARCHAR(30)
 );
 
--- Insert values into tab1
 INSERT INTO tab1 (recordID, name) VALUES
 (1, 'Fred'),
 (2, 'Freda');
-
--- SELECT to display all of the values and columns in table tab1
 SELECT * FROM tab1;
