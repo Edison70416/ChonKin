0b8f52dae1eadec58d7ff4ac32daae97124a0af3
Edison70416 <65762864+Edison70416@users.noreply.github.com>
2023-08-22 22:03:03 +0930

    Update auto_inc_test.sql

--- auto_inc_test.sql
+++ auto_inc_test.sql
@@ -1,10 +1,8 @@
--- Create the test1 database
+
 CREATE DATABASE IF NOT EXISTS test1;
 
--- Use the test1 database
 USE test1;
 
--- Create the table tab1 with auto-increment capability for recordID
 DROP TABLE IF EXISTS tab1;
 CREATE TABLE tab1 (
     recordID INT(11) AUTO_INCREMENT PRIMARY KEY,
